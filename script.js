83bb05d152c68c7b93dede7c576c84b94accb905
Abdul Latif <latif901@hotmail.com>
2022-01-25 12:41:53 +0400

    Load Animation fix

--- script.js
+++ script.js
@@ -1,21 +1,18 @@
-// const scroll = document.querySelectorAll(".scrollObserver")
-
-
-
-// const observer = new IntersectionObserver( 
-//     entries => {
-//         entries.forEach(entry => {
-//             entry.target.classList.toggle("show", entry.isIntersecting)
-//         })
-// }
-// )
-
-// scroll.forEach( scrolls => {
-//     observer.observe(scrolls)
-// })
-
 
 window.addEventListener("load", () =>{
     const preload = document.querySelector(".preload");
     preload.classList.add("loader-finish")
 });
+
+// const landingPage1 = document.querySelector(".landingPage1")
+// const landingPage2 = document.querySelector(".landingPage2")
+
+// landingPage1.addEventListener("click",(e)=>{
+//     console.log(e.layerY);
+// })
+window.addEventListener("scroll", () => {
+    const scrollable = document.documentElement.scrollHeight - window.innerHeight;
+    const scrolled = window.scrollY;
+
+    console.log(scrollable)
+})
